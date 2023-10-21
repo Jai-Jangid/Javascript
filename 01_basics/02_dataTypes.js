e2e362dca77aa138f7a0a4b514ca1fa13064dcb7
Jai-Jangid <148598975+Jai-Jangid@users.noreply.github.com>
2023-10-21 12:08:45 +0000

    datatype

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,25 @@
+"use strict"; // treat all js code in newer version/code
+
+// alert(3+3)    // we are using node js not browser thats why we cant use this alert
+
+console.log(3+3); 
+// code readablity should be high
+
+let name = "jai"
+let age = 23
+let isLoggedIn = false
+
+// primitive data types
+// number => 2 tp power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value which is empty
+// undefined => 
+// symbol => unique
+
+
+// object
+console.log(typeof age);  // integer
+console.log(typeof undefined); // undefined
+console.log(typeof null); //object
