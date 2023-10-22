2357d29e274b7f555340ddd3d6e3b58b26a439b6
Jai-Jangid <148598975+Jai-Jangid@users.noreply.github.com>
2023-10-22 02:44:40 +0000

    operation conversions

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -20,12 +20,54 @@ console.log(booleanIsLoggedIn);
 // 1 => true; 0 => false
 // "" => false
 // "jai" => true
-*/
+
 let someNumber = 33
 
 let stringNumber = String(someNumber)
 console.log(stringNumber);
 console.log(typeof stringNumber);
+*/
 
+// *************** Operations **************
+/*
+let value = 3
+let negValue = -value
+console.log(negValue);
+*/
+/*
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2**3);// 2 is to power 2
+console.log(2/3);
+console.log(2%3);
+*/
+let str1 = "hello"
+let str2 = " jai"
+
+let str3 = str1+str2;
+/*
+console.log(str3);
+
+// complex problem situation like
+console.log("1"+2);
+console.log(1+"2");
+console.log("1"+2+2);
+console.log(1+2+"2");
+        //  if string is staring then it will convert it into string and if string is in last then the first conversion will happen and after that it will add the string like in  colsole.log(1+2+"2") ==> 3+"2" => 32
+
+  */
+ console.log(+true);  // true is a boolean and if we do +true then it will convert in number and do increment due to +(here inceremnt dosent happen because we didnt add any number with it)
+ console.log(+"");  // ==> 0
+
+ let num1 , num2 , num3
+
+ num1 = num2 = num3 = 2 + 2     // this is right but wrong way to write code ,dont use compexity in code use simple way
+
+ let gameCounter = 100
+ gameCounter++;
+ console.log(gameCounter);
+
+// link to read prefix postfix := https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Increment
 
 
