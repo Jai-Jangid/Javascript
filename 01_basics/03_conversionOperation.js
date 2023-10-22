9365df07c2d23aa6add9eebda9000b50e980ef32
Jai-Jangid <148598975+Jai-Jangid@users.noreply.github.com>
2023-10-22 02:07:49 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,31 @@
+let score = null
+/*
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+// valueInNumber = it is used to convert in number
+// "33" => 33
+// "33abs" => NaN (not a number)
+// true => 1; false => 0;
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "jai" => true
+*/
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+
+
