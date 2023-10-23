8fbaaef9e4523a5f1d163b521474975202facd96
Jai-Jangid <148598975+Jai-Jangid@users.noreply.github.com>
2023-10-23 17:14:07 +0000

    dates

---
+++ 01_basics/07_datesinJS.js
@@ -0,0 +1,26 @@
+// Dates
+
+let mydate = new Date()
+// console.log(mydate.toString());
+// console.log(mydate.toDateString());
+// console.log(mydate.toLocaleString());
+// console.log(mydate.toISOString());
+// console.log(typeof mydate);
+
+// let mycreateddate = new Date(2023,0,23)
+// let mycreateddate = new Date(2023,0,23,5,3)
+let mycreateddate = new Date("2023-01-14")
+// console.log(mycreateddate.toLocaleString());
+
+
+let mytimestamp = Date.now()
+
+// console.log(mytimestamp);
+// console.log(mycreateddate.getTime());
+// console.log(Math.floor(Date.now()/1000));
+
+
+let newdate = new Date()
+console.log(newdate);
+console.log(newdate.getMonth());
+console.log(newdate.getDay());
