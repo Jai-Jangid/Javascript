64338994219f7bb40d1b9db75a748619242ac536
Jai-Jangid <148598975+Jai-Jangid@users.noreply.github.com>
2023-10-23 11:52:33 +0000

    num and math

--- 01_basics/06_nums_and_maths.js
+++ 01_basics/06_nums_and_maths.js
@@ -15,10 +15,27 @@ const hundereds = 1000000
 
 
 // **************************Maths*****************************
-
+/*
 console.log(Math);
 console.log(Math.abs(-4));  // used to make negative into positive only
 
 console.log(Math.round(5.6));
 console.log(Math.ceil(4.2));
 console.log(Math.floor(4.2));
+console.log(Math.min(4,2,6,7,1));
+console.log(Math.max(4,2,6,7,1));
+
+*/
+
+console.log(Math.random());
+console.log((Math.random()*10)+1);
+console.log((Math.random()*10)+1);
+// to avoid the stuation of 0.0876543456 type random number which can be zero ,we always use +1 
+
+// if you dont want decimals and want to roundoff the value then you can use math.floor wit it like-
+console.log(Math.floor(Math.random()*10)+1);
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min);
