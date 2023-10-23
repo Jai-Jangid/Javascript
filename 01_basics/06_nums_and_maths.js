b5f7b937904805f5bad3c52685b9b8b544d3ff84
Jai-Jangid <148598975+Jai-Jangid@users.noreply.github.com>
2023-10-23 02:14:01 +0000

    nums and maths

---
+++ 01_basics/06_nums_and_maths.js
@@ -0,0 +1,24 @@
+const score = 400
+// console.log(score);
+const balance = new Number(100)
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(1));
+
+const othernumber = 123.6545
+// console.log(othernumber.toPrecision(4));
+
+
+const hundereds = 1000000
+// console.log(hundereds.toLocaleString('en-In'));
+
+
+// **************************Maths*****************************
+
+console.log(Math);
+console.log(Math.abs(-4));  // used to make negative into positive only
+
+console.log(Math.round(5.6));
+console.log(Math.ceil(4.2));
+console.log(Math.floor(4.2));
