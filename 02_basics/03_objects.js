3d6739477711e18f8f7cf028529ca2397f969465
Jai-Jangid <148598975+Jai-Jangid@users.noreply.github.com>
2023-10-26 01:43:12 +0000

    objects

--- 02_basics/03_objects.js
+++ 02_basics/03_objects.js
@@ -10,7 +10,7 @@ const mysym = Symbol("key1")
 const jsuser = {
     name : "jai",
     "full name" : "jai jangid",
-    mysym : "mykey1"
+    [mysym] : "mykey1",
     age: 18,
     location: "jaipur",
     email: "jai@gmail.com",
@@ -18,10 +18,24 @@ const jsuser = {
     lastLoginDays:["Monday","Saturday"]
 }
 
-console.log(jsuser.email);
-console.log(jsuser["email"])
-console.log(jsuser["full name"])
-console.log(typeof jsuser.mysym)
+// console.log(jsuser.email);
+// console.log(jsuser["email"])
+// console.log(jsuser["full name"])
+// console.log(jsuser[mysym])
 
 
-myarray = ["j","a"]
+jsuser.email = "jai123@gmail.com"
+// Object.freeze(jsuser)   // freeze method is used to freeze the email ,it wont allowed anyone to change it after applying freeze method
+// jsuser.email = "jaiTCS@gmail.com"
+// console.log(jsuser);
+
+
+jsuser.greeting = function(){
+    console.log("hello js user");
+}
+jsuser.greeting2 = function(){
+    console.log(`hello js user,${this.name}`);
+}
+
+console.log(jsuser.greeting());
+console.log(jsuser.greeting2());
