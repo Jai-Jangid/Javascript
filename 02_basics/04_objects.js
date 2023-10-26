a7a9ff7622c415f22493e13196c47f080b1a990a
Jai-Jangid <148598975+Jai-Jangid@users.noreply.github.com>
2023-10-26 11:01:33 +0000

    objects & functions

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -28,3 +28,27 @@ const obj4 = {5:"a", 6:"b"}
 const obj3 = {...obj1, ...obj2, ...obj4}
 // console.log(obj3);
 
+const users= [
+    {
+        id: 1,
+        email: "j@gmail.com"
+    },
+    {
+        id: 1,
+        email: "j@gmail.com"
+    },
+    {
+        id: 1,
+        email: "j@gmail.com"
+    },
+
+]
+
+users[1].email
+console.log(tinderuser);
+
+console.log(Object.keys(tinderuser));
+console.log(Object.values(tinderuser));
+console.log(Object.entries(tinderuser));
+
+console.log(tinderuser.hasOwnProperty('isLoggedIn'));
