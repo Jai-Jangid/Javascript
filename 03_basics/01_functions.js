94b135988666dad788ef6c3be2312680d5ffaa44
Jai-Jangid <148598975+Jai-Jangid@users.noreply.github.com>
2023-10-26 16:47:28 +0000

    functions

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -40,4 +40,35 @@ function loginusermessage(username){
 }
 
 // console.log(loginusermessage("rahul"));
-console.log(loginusermessage());
+// console.log(loginusermessage());
+
+
+function calculatecartprice(val1,val2,...num1){
+        return num1
+}
+
+// console.log(calculatecartprice(200, 300, 400,50000));
+
+
+const user = {
+    username: "jai",
+    price: 199
+}
+
+function handleObject(anyobject){
+    console.log(`username is ${anyobject.username} and price is ${anyobject.price}`);
+}
+
+// handleObject(user)
+handleObject({
+    username: "sam",
+    price: 1923
+})
+
+const mynewarray = [200,300,400,110]
+
+function retrunsecondvalue(getArray){
+    return getArray[1]
+}
+
+console.log(retrunsecondvalue(mynewarray));
