20adbd048dd1e7c66f4f8701d2208629cd6eb8a9
Jai-Jangid <148598975+Jai-Jangid@users.noreply.github.com>
2023-10-27 05:40:46 +0000

    arrow

--- 03_basics/03_arrow.js
+++ 03_basics/03_arrow.js
@@ -27,6 +27,33 @@ const chai = function(){
     let username = "jai"
     console.log(this.username);
 }
+chai()
 */
+/*
+const chai = () => {
+    let username = "jai"
+    console.log(this);
+}
 
 chai()
+*/
+
+// //basic arrow function=>
+// // explicit return (when you use paranthises'{}')
+// const addtwo = (num1, num2) => {
+//     return num1+num2
+// }
+
+//implicit return=>
+// const addtwo = (num1, num2) =>  num1+num2
+// const addtwo = (num1, num2) =>  (num1+num2)
+// if you use '{}' paranthises then you have to write return but if you are using '()' then you dont have to write return like implicit return shown above 
+
+const addtwo = (num1, num2) =>  ({username: "jai"})
+
+console.log(addtwo(3,4));
+
+
+const myarray = [2,3,4,65,6]
+
+// myarray.forEach()
