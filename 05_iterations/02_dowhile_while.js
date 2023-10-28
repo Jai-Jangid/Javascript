f0090e00e97c089dee5b08b3a3ade76d5b38f165
Jai-Jangid <148598975+Jai-Jangid@users.noreply.github.com>
2023-10-28 05:15:12 +0000

    while,dowhile loop

---
+++ 05_iterations/02_dowhile_while.js
@@ -0,0 +1,28 @@
+// While loop
+/*
+let index = 0
+
+while (index<=10) {
+    console.log(`value of index is ${index}`);
+    index = index+2
+}
+
+let myarray = ['batman', 'flash', 'superman']
+
+let arr = 0
+while (arr<myarray.length) {
+    console.log(`value is ${myarray[arr]}`);
+    arr = arr +1
+}
+*/
+
+// Do-While loop
+/*
+let score = 1
+
+do {
+    console.log(`score is ${score}`);
+    score++
+} while (score<=10);
+
+*/
